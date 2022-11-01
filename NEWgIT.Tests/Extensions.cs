9a4e3215561e76593a4a4508f2978a8b28da1093
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-01 12:56:24 +0100

    stuff

--- NEWgIT.Tests/Extensions.cs
+++ NEWgIT.Tests/Extensions.cs
@@ -1,22 +1,23 @@
 public static class Extensions
 {
-    public static void Delete(this Repository repo)
-    {
-        DeleteReadOnlyDirectory(repo.Info.Path);
-    }
+    // public static void ForceDelete(this Repository repo)
 
-    private static void DeleteReadOnlyDirectory(string directory)
-    {
-        foreach (var subdirectory in Directory.EnumerateDirectories(directory))
-        {
-            DeleteReadOnlyDirectory(subdirectory);
-        }
-        foreach (var fileName in Directory.EnumerateFiles(directory))
-        {
-            var fileInfo = new FileInfo(fileName);
-            fileInfo.Attributes = FileAttributes.Normal;
-            fileInfo.Delete();
-        }
-        Directory.Delete(directory);
-    }
+    // {
+    //     DeleteReadOnlyDirectory(repo.Info.Path);
+    // }
+
+    // private static void DeleteReadOnlyDirectory(string directory)
+    // {
+    //     foreach (var subdirectory in Directory.EnumerateDirectories(directory))
+    //     {
+    //         DeleteReadOnlyDirectory(subdirectory);
+    //     }
+    //     foreach (var fileName in Directory.EnumerateFiles(directory))
+    //     {
+    //         var fileInfo = new FileInfo(fileName);
+    //         fileInfo.Attributes = FileAttributes.Normal;
+    //         fileInfo.Delete();
+    //     }
+    //     Directory.Delete(directory);
+    // }
 }

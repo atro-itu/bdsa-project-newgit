95f23d29c8b4c078a462d2e89b89abb55869a811
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-01 20:50:59 +0100

    remove comments

--- NEWgIT.Tests/Extensions.cs
+++ NEWgIT.Tests/Extensions.cs
@@ -1,26 +1,5 @@
 public static class Extensions
 {
-    // public static void ForceDelete(this Repository repo)
-
-    // {
-    //     DeleteReadOnlyDirectory(repo.Info.Path);
-    // }
-
-    // private static void DeleteReadOnlyDirectory(string directory)
-    // {
-    //     foreach (var subdirectory in Directory.EnumerateDirectories(directory))
-    //     {
-    //         DeleteReadOnlyDirectory(subdirectory);
-    //     }
-    //     foreach (var fileName in Directory.EnumerateFiles(directory))
-    //     {
-    //         var fileInfo = new FileInfo(fileName);
-    //         fileInfo.Attributes = FileAttributes.Normal;
-    //         fileInfo.Delete();
-    //     }
-    //     Directory.Delete(directory);
-    // }
-
     public static void Seed(this Repository repo)
     {
 

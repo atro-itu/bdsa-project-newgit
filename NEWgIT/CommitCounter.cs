02e07a2bc0b3c06a00d49f43e740622ab241e9a9
Frederik Petersen <43568735+fredpetersen@users.noreply.github.com>
2022-11-02 16:59:47 +0100

    change author mode to find all authors (#19)
    * change author mode to find all authors
    
    * Simplify AuthorMode

--- NEWgIT/CommitCounter.cs
+++ NEWgIT/CommitCounter.cs
@@ -5,6 +5,18 @@ public static class CommitCounter
     public static Dictionary<DateOnly, int> FrequencyMode(ICommitLog log) =>
         log.GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
 
-    public static Dictionary<DateOnly, int> AuthorMode(ICommitLog log, string author) =>
-        log.Where(c => c.Author.Name == author).GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
+    public static Dictionary<string, Dictionary<DateOnly, int>> AuthorMode(ICommitLog log)
+    {
+        var authors = log.DistinctBy(c => c.Author.Name).Select(c => c.Author.Name);
+        var result = new Dictionary<string, Dictionary<DateOnly, int>>();
+        foreach (var author in authors)
+        {
+            result.Add(author, FrequencyByAuthor(log, author));
+        }
+        return result;
+    }
+
+    public static Dictionary<DateOnly, int> FrequencyByAuthor(ICommitLog log, string author) =>
+       log.Where(c => c.Author.Name == author).GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
+
 }

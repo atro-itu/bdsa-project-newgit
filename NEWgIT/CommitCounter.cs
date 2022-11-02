b3262783b8b661df296f767a2431741222cc5ef6
Frederik Petersen <fred.p@live.dk>
2022-11-02 16:10:52 +0100

    fix whitespace

--- NEWgIT/CommitCounter.cs
+++ NEWgIT/CommitCounter.cs
@@ -5,5 +5,6 @@ public static class CommitCounter
     public static Dictionary<DateOnly, int> FrequencyMode(ICommitLog log) =>
         log.GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
 
-    public static Dictionary<DateOnly, int> AuthorMode(ICommitLog log, string author) => log.Where(c => c.Author.Name == author).GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
+    public static Dictionary<DateOnly, int> AuthorMode(ICommitLog log, string author) =>
+        log.Where(c => c.Author.Name == author).GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
 }

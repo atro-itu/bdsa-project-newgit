142d233c316b57f10e0624311eb1485c214a81e8
Frederik Petersen <43568735+fredpetersen@users.noreply.github.com>
2022-11-02 15:46:58 +0100

    added tests for author and frequencymode (#16)
    * added tests for author and frequencymode
    
    * re-add newline

--- NEWgIT/CommitCounter.cs
+++ NEWgIT/CommitCounter.cs
@@ -2,12 +2,12 @@ namespace NEWgIT;
 
 public static class CommitCounter
 {
-    public static void FrequencyMode()
+    public static Dictionary<DateOnly, int> FrequencyMode(ICommitLog log)
     {
         throw new NotImplementedException();
     }
 
-    public static void AuthorMode()
+    public static Dictionary<DateOnly, int> AuthorMode(ICommitLog log, string author)
     {
         throw new NotImplementedException();
     }

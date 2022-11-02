8708de79d4693eb1a2e8ecd5f83b05890b52805f
Frederik Petersen <fred.p@live.dk>
2022-11-02 16:25:56 +0100

    change author mode to find all authors

--- NEWgIT.Tests/CommitCounterTests.cs
+++ NEWgIT.Tests/CommitCounterTests.cs
@@ -16,7 +16,7 @@ public class CommitCounterTests : IDisposable
     }
 
     [Fact]
-    public void FrequencyMode_Should_Return_Sum_Of_Commits_Per_Day()
+    public void FrequencyMode_Should_Return_Collective_Commit_Frequency()
     {
         //Arrange
         var log = _repository.Commits;
@@ -37,14 +37,14 @@ public class CommitCounterTests : IDisposable
     [InlineData("Lucas", new int[] { 2, 0, 0, 2, 0 })]
     [InlineData("Bank", new int[] { 1, 0, 0, 1, 2 })]
     [InlineData("Trøstrup", new int[] { 0, 1, 1, 0, 0 })]
-    public void AuthorMode_Should_Return_Sum_Of_Commits_Per_Author(string author, int[] expected)
+    public void FrequencyByAuthor_Should_Return_Commit_Frequency_By_Author(string author, int[] expected)
     {
         //Arrange
         var log = _repository.Commits;
         var Dates = new DateOnly[] { DateOnly.FromDateTime(DateTimeOffset.Now.Date), new DateOnly(2010, 5, 25), new DateOnly(2010, 5, 26), new DateOnly(2019, 5, 25), new DateOnly(2019, 5, 26) };
 
         //Act
-        var result = CommitCounter.AuthorMode(log, author);
+        var result = CommitCounter.FrequencyByAuthor(log, author);
 
         //Assert
         for (int i = 0; i < Dates.Length; i++)
@@ -52,6 +52,31 @@ public class CommitCounterTests : IDisposable
             if (expected[i] == 0) continue;
             result[Dates[i]].Should().Be(expected[i]);
         }
+    }
+
+    [Fact]
+    public void AuthorMode_Should_Return_Commit_Frequency_For_Authors()
+    {
+        //Arrange
+        var log = _repository.Commits;
+
+        //Act
+        var result = CommitCounter.AuthorMode(log);
+
+        //Assert
+        result.Count().Should().Be(3);
+        result["Lucas"].Count().Should().Be(2);
+        result["Lucas"][DateOnly.FromDateTime(DateTimeOffset.Now.Date)].Should().Be(2);
+        result["Lucas"][new DateOnly(2019, 5, 25)].Should().Be(2);
+
+        result["Bank"].Count().Should().Be(3);
+        result["Bank"][DateOnly.FromDateTime(DateTimeOffset.Now.Date)].Should().Be(1);
+        result["Bank"][new DateOnly(2019, 5, 25)].Should().Be(1);
+        result["Bank"][new DateOnly(2019, 5, 26)].Should().Be(2);
+
+        result["Trøstrup"].Count().Should().Be(2);
+        result["Trøstrup"][new DateOnly(2010, 5, 25)].Should().Be(1);
+        result["Trøstrup"][new DateOnly(2010, 5, 26)].Should().Be(1);
     }
 
     public void Dispose()

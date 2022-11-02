b614de101db1bb998afe5e343c7bee533cd54da9
Frederik Petersen <fred.p@live.dk>
2022-11-02 16:08:31 +0100

    Implement CommitCounter

--- NEWgIT.Tests/CommitCounterTests.cs
+++ NEWgIT.Tests/CommitCounterTests.cs
@@ -25,21 +25,23 @@ public class CommitCounterTests : IDisposable
         var result = CommitCounter.FrequencyMode(log);
 
         //Assert
-        result.Count.Should().Be(3);
-        result[new DateOnly(2019, 5, 25)].Should().Be(4);
-        result[new DateOnly(2019, 5, 26)].Should().Be(3);
+        result.Count().Should().Be(5);
+        result[new DateOnly(2010, 05, 25)].Should().Be(1);
+        result[new DateOnly(2010, 05, 26)].Should().Be(1);
+        result[new DateOnly(2019, 5, 25)].Should().Be(3);
+        result[new DateOnly(2019, 5, 26)].Should().Be(2);
         result[DateOnly.FromDateTime(DateTimeOffset.Now.Date)].Should().Be(3);
     }
 
     [Theory]
-    [InlineData("Lucas", new int[] { 2, 2, 0 })]
-    [InlineData("Bank", new int[] { 1, 1, 2 })]
-    [InlineData("Trøstrup", new int[] { 0, 1, 1 })]
+    [InlineData("Lucas", new int[] { 2, 0, 0, 2, 0 })]
+    [InlineData("Bank", new int[] { 1, 0, 0, 1, 2 })]
+    [InlineData("Trøstrup", new int[] { 0, 1, 1, 0, 0 })]
     public void AuthorMode_Should_Return_Sum_Of_Commits_Per_Author(string author, int[] expected)
     {
         //Arrange
         var log = _repository.Commits;
-        var Dates = new DateOnly[] { DateOnly.FromDateTime(DateTimeOffset.Now.Date), new DateOnly(2019, 5, 25), new DateOnly(2019, 5, 26) };
+        var Dates = new DateOnly[] { DateOnly.FromDateTime(DateTimeOffset.Now.Date), new DateOnly(2010, 5, 25), new DateOnly(2010, 5, 26), new DateOnly(2019, 5, 25), new DateOnly(2019, 5, 26) };
 
         //Act
         var result = CommitCounter.AuthorMode(log, author);

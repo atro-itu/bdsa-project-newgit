e7aae6342aba5ad42ce7d0230437889f1db4dbf1
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-07 13:41:23 +0100

    fix format

--- NEWgIT/CommitCounter.cs
+++ NEWgIT/CommitCounter.cs
@@ -8,8 +8,6 @@ public static class CommitCounter
     public static Dictionary<string, Dictionary<DateOnly, int>> AuthorMode(ICommitLog log) =>
         log.DistinctBy(c => c.Author.Name).Select(c => c.Author.Name).ToDictionary(author => author, author => FrequencyByAuthor(log, author));
 
-
     public static Dictionary<DateOnly, int> FrequencyByAuthor(ICommitLog log, string author) =>
        log.Where(c => c.Author.Name == author).GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
-
 }

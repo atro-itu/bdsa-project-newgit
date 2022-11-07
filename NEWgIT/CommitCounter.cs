d139ed483f239beee779e45fd4e4a6588840d9a4
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-07 13:33:13 +0100

    use lambda for author mode

--- NEWgIT/CommitCounter.cs
+++ NEWgIT/CommitCounter.cs
@@ -5,16 +5,9 @@ public static class CommitCounter
     public static Dictionary<DateOnly, int> FrequencyMode(ICommitLog log) =>
         log.GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());
 
-    public static Dictionary<string, Dictionary<DateOnly, int>> AuthorMode(ICommitLog log)
-    {
-        var authors = log.DistinctBy(c => c.Author.Name).Select(c => c.Author.Name);
-        var result = new Dictionary<string, Dictionary<DateOnly, int>>();
-        foreach (var author in authors)
-        {
-            result.Add(author, FrequencyByAuthor(log, author));
-        }
-        return result;
-    }
+    public static Dictionary<string, Dictionary<DateOnly, int>> AuthorMode(ICommitLog log) =>
+        log.DistinctBy(c => c.Author.Name).Select(c => c.Author.Name).ToDictionary(author => author, author => FrequencyByAuthor(log, author));
+
 
     public static Dictionary<DateOnly, int> FrequencyByAuthor(ICommitLog log, string author) =>
        log.Where(c => c.Author.Name == author).GroupBy(c => DateOnly.FromDateTime(c.Committer.When.Date)).ToDictionary(g => g.Key, g => g.Count());

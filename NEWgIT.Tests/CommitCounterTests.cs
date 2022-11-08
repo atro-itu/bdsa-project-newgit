a0c92da6ee421c2ad97d1e30b8ec5af63b4c35ad
Frederik Petersen <43568735+fredpetersen@users.noreply.github.com>
2022-11-08 14:15:20 +0100

    remove current date from tests (#28)
    * remove current date from tests
    
    * fix comments

--- NEWgIT.Tests/CommitCounterTests.cs
+++ NEWgIT.Tests/CommitCounterTests.cs
@@ -26,11 +26,11 @@ public class CommitCounterTests : IDisposable
 
         //Assert
         result.Count().Should().Be(5);
+        result[new DateOnly(2008, 02, 29)].Should().Be(3);
         result[new DateOnly(2010, 05, 25)].Should().Be(1);
         result[new DateOnly(2010, 05, 26)].Should().Be(1);
         result[new DateOnly(2019, 5, 26)].Should().Be(2);
         result[new DateOnly(2019, 5, 25)].Should().Be(3);
-        result[DateOnly.FromDateTime(DateTimeOffset.Now.Date)].Should().Be(3);
     }
 
     [Theory]
@@ -41,7 +41,7 @@ public class CommitCounterTests : IDisposable
     {
         //Arrange
         var log = _repository.Commits;
-        var Dates = new DateOnly[] { DateOnly.FromDateTime(DateTimeOffset.Now.Date), new DateOnly(2010, 5, 25), new DateOnly(2010, 5, 26), new DateOnly(2019, 5, 25), new DateOnly(2019, 5, 26) };
+        var Dates = new DateOnly[] { new DateOnly(2008, 02, 29), new DateOnly(2010, 5, 25), new DateOnly(2010, 5, 26), new DateOnly(2019, 5, 25), new DateOnly(2019, 5, 26) };
 
         //Act
         var result = CommitCounter.FrequencyByAuthor(log, author);
@@ -66,11 +66,11 @@ public class CommitCounterTests : IDisposable
         //Assert
         result.Count().Should().Be(3);
         result["Lucas"].Count().Should().Be(2);
-        result["Lucas"][DateOnly.FromDateTime(DateTimeOffset.Now.Date)].Should().Be(2);
+        result["Lucas"][new DateOnly(2008, 02, 29)].Should().Be(2);
         result["Lucas"][new DateOnly(2019, 5, 25)].Should().Be(2);
 
         result["Bank"].Count().Should().Be(3);
-        result["Bank"][DateOnly.FromDateTime(DateTimeOffset.Now.Date)].Should().Be(1);
+        result["Bank"][new DateOnly(2008, 02, 29)].Should().Be(1);
         result["Bank"][new DateOnly(2019, 5, 25)].Should().Be(1);
         result["Bank"][new DateOnly(2019, 5, 26)].Should().Be(2);
 

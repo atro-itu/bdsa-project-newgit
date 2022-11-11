13140a003fec5a996f8f6e818db2b27e75fae6db
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-11 14:13:03 +0100

    add postgres config

--- NEWgIT/Usings.cs
+++ NEWgIT/Usings.cs
@@ -3,3 +3,4 @@ global using NEWgIT.Infrastructure;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.Extensions.Configuration;
+global using NEWgIT.Core;

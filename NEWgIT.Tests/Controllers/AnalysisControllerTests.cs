6e07e8aca41deb1aded1b3f55b0d2ea45d89849f
Lucasfth <luha@itu.dk>
2022-11-16 11:10:28 +0100

    add all Get tests
    iss #39

--- NEWgIT.Tests/Controllers/AnalysisControllerTests.cs
+++ NEWgIT.Tests/Controllers/AnalysisControllerTests.cs
@@ -19,7 +19,7 @@ public class AnalysisControllerTests
     }
 
     [Fact]
-    public void Get_Should_Return()
+    public void Get_Should_Return_Repo_As_AuthorMode_Given_RepoIdentifier()
     {
         // Arrange
         List<CommitDTO> commitDTO = new List<CommitDTO>()
@@ -41,4 +41,63 @@ public class AnalysisControllerTests
         actual.Should().BeEquivalentTo(expected);
 
     }
+
+    [Fact]
+    public void Get_Should_Return_Repo_As_FrequencyMode_Given_RepoIdentifier_And_FrequencyMode_String()
+    {
+        // Arrange
+        List<CommitDTO> commitDTO = new List<CommitDTO>()
+        {
+            new CommitDTO(1, "Frepe", new System.DateTime(2021, 1, 1), "1234567890"),
+            new CommitDTO(2, "Banksy", new System.DateTime(2021, 5, 2), "1234567891"),
+        };
+        var analysisDTO = new AnalysisDTO(1, "duckth/testrepo", commitDTO, "1234567891");
+        _mockRepository.FindByIdentifier("duckth/testrepo").Returns<AnalysisDTO>(analysisDTO);
+        var expected = new OkObjectResult("{\"2021-01-01\":1,\"2021-05-02\":1}")
+        {
+            ContentTypes = { "application/json" }
+        };
+
+        // Act
+        var actual = _controller.Get("duckth", "testrepo", "frequency").Result;
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Get_Should_Return_NotFoundObjectResult_Given_None_Existing_Repo()
+    {
+        // Arrange
+        _mockRepository.FindByIdentifier("duckth/testrepo").Returns<AnalysisDTO>(i => null!); // (i => null!) is a hack to make it compile
+        var expected = new NotFoundObjectResult(null);
+
+        // Act
+        var actual = _controller.Get("duckth", "testrepo").Result;
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Get_Should_Return_BadRequestObjectResult_Given_IncorrectString()
+    {
+        // Arrange
+        List<CommitDTO> commitDTO = new List<CommitDTO>()
+        {
+            new CommitDTO(1, "Frepe", new System.DateTime(2021, 1, 1), "1234567890"),
+            new CommitDTO(2, "Banksy", new System.DateTime(2021, 5, 2), "1234567891"),
+        };
+        var analysisDTO = new AnalysisDTO(1, "duckth/testrepo", commitDTO, "1234567891");
+        _mockRepository.FindByIdentifier("duckth/testrepo").Returns<AnalysisDTO>(analysisDTO);
+        var expected = new BadRequestObjectResult("Invalid mode");
+
+        // Act
+        var actual = _controller.Get("duckth", "testrepo", "definetly incorrect").Result;
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    
 }

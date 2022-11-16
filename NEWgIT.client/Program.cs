282af058155b65927eac407d290f934b9b076846
Fefa1999 <fefa@itu.dk>
2022-11-16 17:04:14 +0100

    Frontend
    Co-authored-by: ChrBank <ChrBank@users.noreply.github.com>
    Co-authored-by: Mads0709 <Mads0709@users.noreply.github.com>

--- NEWgIT.client/Program.cs
+++ NEWgIT.client/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using client.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
+//builder.Services.AddHttpClient( );
 
 var app = builder.Build();
 
--- NEWgIT/Controllers/AnalysisController.cs
+++ NEWgIT/Controllers/AnalysisController.cs
@@ -1,7 +1,4 @@
 namespace NEWgIT.Api.Controllers;
-
-using System.Net;
-using System.Text;
 using NEWgIT.Core;
 
 [ApiController]
@@ -18,96 +15,24 @@ public class AnalysisController : ControllerBase
     }
 
     [HttpGet]
-    [Route("{repoOwner}/{repoName}")]
-    public ActionResult<string> Get(string repoOwner, string repoName, string? mode = "author")
+    [Route("analysis/{repoOwner}/{repoName}")]
+    public string Get(string repoOwner, string repoName)
     {
-        var analysis = _repository.FindByIdentifier($"{repoOwner.ToLower()}/{repoName.ToLower()}");
-        if (analysis == null) return new NotFoundObjectResult(null);
-        if (!(mode == "author" || mode == "frequency")) return new BadRequestObjectResult("Invalid mode");
-        var output = mode switch
-        {
-            "author" => JsonConvert.SerializeObject(CommitCounter.AuthorMode(analysis.commits)),
-            "frequency" => JsonConvert.SerializeObject(CommitCounter.FrequencyMode(analysis.commits)),
-            _ => throw new NotImplementedException()
-        };
-
-        return new OkObjectResult(output)
-        {
-            ContentTypes = { "application/json" }
-        };
-
+        string sourceUrl = $"https://github.com/{repoOwner}/{repoName}";
+        (HashSet<CommitCreateDTO> commits, string latestCommitHash) = CommitFetcherService.Instance.GetRepoCommits(sourceUrl);
+        return JsonConvert.SerializeObject(commits);
     }
 
     [HttpGet(Name = "ReadAnalysis")]
-    public ActionResult<string> Read(string? mode)
+    public string Read()
     {
-        throw new NotImplementedException();
-        // IReadOnlyCollection<AnalysisDTO> analysis = _repository.Read();
-
-        // if (!(mode == "author" || mode == "frequency")) return new BadRequestObjectResult("Invalid mode");
-
-        // var output = analysis.Select(a => new
-        // {
-        //     identifier = a.repoIdentifier,
-        //     commits = mode switch
-        //     {
-        //         "author" => CommitCounter.AuthorMode(a.commits),
-        //         "frequency" => CommitCounter.FrequencyMode(a.commits),
-        //         _ => throw new NotImplementedException()
-        //     }
-        // });
-
-
-        // return new OkObjectResult(JsonConvert.SerializeObject(output))
-        // {
-        //     ContentTypes = { "application/json" }
-        // };
-    }
-
-    [HttpPost]
-    [Route("{repoOwner}/{repoName}")]
-    public ActionResult Create(string repoOwner, string repoName)
-    {
-        var repoIdentifier = $"{repoOwner.ToLower()}/{repoName.ToLower()}";
-        var analysis = _repository.FindByIdentifier(repoIdentifier);
-        if (analysis != null) return new ConflictObjectResult(new { message = "Analysis already exists" });
-
-        var sourceUrl = GetSourceUrl(repoIdentifier);
-        var (commits, hash) = CommitFetcherService.Instance.GetRepoCommits(sourceUrl);
-
-        var (response, analysisId) = _repository.Create(new AnalysisCreateDTO(repoIdentifier, commits, hash));
-
-        return new CreatedResult(repoIdentifier, null);
+        IReadOnlyCollection<AnalysisDTO> analysis = _repository.Read();
+        return JsonConvert.SerializeObject(analysis);
     }
 
     [HttpPut]
-    [Route("{repoOwner}/{repoName}")]
-    public IActionResult Update(string repoOwner, string repoName)
+    [Route("analysis/{repoOwner}/{repoName}")]
+    public string Update(string repoOwner, string repoName)
     {
-        var repoIdentifier = $"{repoOwner.ToLower()}/{repoName.ToLower()}";
-
-        var sourceUrl = GetSourceUrl(repoIdentifier);
-        var (commits, hash) = CommitFetcherService.Instance.GetRepoCommits(sourceUrl);
-
-        var updateDTO = new AnalysisUpdateDTO(repoIdentifier, commits, hash);
-        var response = _repository.Update(updateDTO);
-
-        if (response == Core.Response.NotFound) return new NotFoundObjectResult(null);
-        if (response == Core.Response.NotModified) return new NoContentResult();
-        return new OkObjectResult(null);
-
-    }
-
-    [HttpDelete]
-    [Route("{repoOwner}/{repoName}")]
-    public IActionResult Delete(string repoOwner, string repoName)
-    {
-        var repoIdentifier = $"{repoOwner.ToLower()}/{repoName.ToLower()}";
-        var deleteDTO = new AnalysisDeleteDTO(repoIdentifier);
-        var response = _repository.Delete(deleteDTO);
-        if (response == Core.Response.NotFound) return new NotFoundObjectResult(null);
-        return new NoContentResult();
-    }
-
-    private string GetSourceUrl(string repoIdentifier) => $"https://github.com/{repoIdentifier}";
-}
+        return "not implemented";
+    }}

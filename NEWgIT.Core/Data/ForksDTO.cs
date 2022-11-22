4010aa6e765d497cb855804ff95f403bd130a64f
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-22 10:47:55 +0100

    Update NEWgIT.Core/Data/ForksDTO.cs
    Co-authored-by: Frederik Petersen <43568735+fredpetersen@users.noreply.github.com>

--- NEWgIT.Core/Data/ForksDTO.cs
+++ NEWgIT.Core/Data/ForksDTO.cs
@@ -3,5 +3,5 @@ namespace NEWgIT.Core.Data;
 /// <summary>
 /// Represents a collection of forks.
 /// </summary>
-/// <param name="Forks">The forks.</param>
+/// <param name="Forks">A collection of forks, represented by their repository identifier (<repoOwner>/<repoName>).</param>
 public record ForksDTO(ICollection<string> Forks);

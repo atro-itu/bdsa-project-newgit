ae48a88810c0fd10a4eee9874dabff69ad734520
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-11-30 21:52:02 +0100

    move api settings to conf

--- NEWgIT.Client/Program.cs
+++ NEWgIT.Client/Program.cs
@@ -5,15 +5,19 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var ApiBaseAddress = builder.Configuration.GetValue<string>("NewgitApi:BaseAddress")!;
+var ApiAccessScope = builder.Configuration.GetValue<string>("NewgitApi:AccessScopeUrl")!;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("WebAPI",
-        client => client.BaseAddress = new Uri(Constants.ApiBaseAddress))
+        client => client.BaseAddress = new Uri(ApiBaseAddress))
     .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
         .ConfigureHandler(
-            authorizedUrls: new[] { Constants.ApiBaseAddress },
-            scopes: new[] { Constants.ApiAccessScope }));
+            authorizedUrls: new[] { ApiBaseAddress },
+            scopes: new[] { ApiAccessScope }));
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
     .CreateClient("WebAPI"));
@@ -23,6 +27,6 @@ builder.Services.AddMsalAuthentication(options =>
     builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
-    options.ProviderOptions.DefaultAccessTokenScopes.Add(Constants.ApiAccessScope);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(ApiAccessScope);
 });
 await builder.Build().RunAsync();
